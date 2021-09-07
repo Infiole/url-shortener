8327e97be62b9acb45c78ecf84bed2dfa6fcd544
Tan Yu Yang <tyy356@gmail.com>
2021-09-08 00:09:25 +0800

    Update tests

--- shrinker/tests.py
+++ shrinker/tests.py
@@ -1,10 +1,11 @@
 from django.http import response
-from django.test import Client, TestCase
+from django.test import Client, TestCase, SimpleTestCase
 from shrinker.models import ShortURL
 
 # Create your tests here.
 class ShortURLTestCase(TestCase):
   def setUp(self):
+    # Creates and saves ShortURL object to database
     url_object = ShortURL(long_url='https://www.google.com')
     url_object.save()
     self.short_url_object = url_object
@@ -16,5 +17,9 @@ class ShortURLTestCase(TestCase):
     assert response.status_code == 200
 
   def test_redirect(self):
+    # Retrieves long url from database and redirects user
     response = self.client.get(f'/{self.short_url_object.short_url}')
-    assert response.status_code == 302
+    long_url = self.short_url_object.long_url
+    SimpleTestCase.assertRedirects(self, response=response, expected_url=long_url, 
+                                   status_code=302, target_status_code=302, 
+                                   msg_prefix='', fetch_redirect_response=True)
